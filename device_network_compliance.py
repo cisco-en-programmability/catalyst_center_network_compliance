1cb1e5eb4c39fd23b749b3b6e35651fc2e26b419
gzapodea <gzapodea@cisco.com>
2023-11-27 15:35:14 -0800

    initial commit

---
+++ device_network_compliance.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2023 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
+__author__ = "Gabriel Zapodeanu TME, ENB"
+__email__ = "gzapodea@cisco.com"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2023 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+import logging
+import os
+import time
+import json
+import yaml
+import base64
+import github_apis
+import difflib
+
+from github import Github
+from datetime import datetime
+
+from dnacentersdk import DNACenterAPI
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth  # for Basic Auth
+from pprint import pprint
+
+
+load_dotenv('environment.env')
+
+CATALYST_CENTER_URL = os.getenv('CATALYST_CENTER_URL')
+CATALYST_CENTER_USER = os.getenv('CATALYST_CENTER_USER')
+CATALYST_CENTER_PASS = os.getenv('CATALYST_CENTER_PASS')
+
+GITHUB_USERNAME = os.getenv('GITHUB_USERNAME')
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+GITHUB_REPO = os.getenv('GITHUB_REPO')
+
+NETWORK_CONFIGS_PATH = 'network_configs/'
+
+os.environ['TZ'] = 'America/Los_Angeles'  # define the timezone for PST
+time.tzset()  # adjust the timezone, more info https://help.pythonanywhere.com/pages/SettingTheTimezone/
+
+CATALYST_CENTER_AUTH = HTTPBasicAuth(CATALYST_CENTER_USER, CATALYST_CENTER_PASS)
+FILE_NAME = 'custom_network_compliance.yaml'
+
+
+def compare_configs(cfg1, cfg2):
+    """
+    This function, using the unified diff function, will compare two config files and identify the changes.
+    '+' or '-' will be prepended in front of the lines with changes
+    :param cfg1: old configuration file path and filename
+    :param cfg2: new configuration file path and filename
+    :return: text with the configuration lines that changed. The return will include the configuration for the sections
+    that include the changes
+    """
+
+    # open the old and new configuration fiels
+    f1 = open(cfg1, 'r')
+    old_cfg = f1.readlines()
+    f1.close()
+
+    f2 = open(cfg2, 'r')
+    new_cfg = f2.readlines()
+    f2.close()
+
+    # compare the two specified config files {cfg1} and {cfg2}
+    d = difflib.unified_diff(old_cfg, new_cfg, n=9)
+
+    # create a diff_list that will include all the lines that changed
+    # create a diff_output string that will collect the generator output from the unified_diff function
+    diff_list = []
+    diff_output = ''
+
+    for line in d:
+        diff_output += line
+        if line.find('Current configuration') == -1:
+            if line.find('Last configuration change') == -1:
+                if (line.find('+++') == -1) and (line.find('---') == -1):
+                    if (line.find('-!') == -1) and (line.find('+!') == -1):
+                        if line.startswith('+'):
+                            diff_list.append('\n' + line)
+                        elif line.startswith('-'):
+                            diff_list.append('\n' + line)
+
+    # process the diff_output to select only the sections between '!' characters for the sections that changed,
+    # replace the empty '+' or '-' lines with space
+    diff_output = diff_output.replace('+!', '!')
+    diff_output = diff_output.replace('-!', '!')
+    diff_output_list = diff_output.split('!')
+
+    all_changes = []
+
+    for changes in diff_list:
+        for config_changes in diff_output_list:
+            if changes in config_changes:
+                if config_changes not in all_changes:
+                    all_changes.append(config_changes)
+
+    # create a config_text string with all the sections that include changes
+    config_text = ''
+    for items in all_changes:
+        config_text += items
+
+    return config_text
+
+
+def main():
+    """
+    This app will pull custom configuration rules from GitHub and identify if all devices are compliant with the rules.
+    """
+
+    # logging, debug level, to file {application_run.log}
+    logging.basicConfig(level=logging.INFO)
+
+    current_time = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    logging.info(' Application "device_network_compliance.py" Start, ' + current_time)
+
+    # get the repos for user
+    repos = github_apis.get_private_repos()
+
+    # verify if repo exists
+    if GITHUB_REPO not in repos:
+        logging.info(' Repo "' + GITHUB_REPO + '" not found!')
+        return
+    logging.info(' Repo "' + GITHUB_REPO + '" found!')
+
+    # get the network settings intent file
+    file_content = github_apis.get_repo_file_content(username=GITHUB_USERNAME, repo_name=GITHUB_REPO, file_name=FILE_NAME)
+    logging.info(' File "' + FILE_NAME + '" found!')
+
+    # decode the YAMl file
+    intent_config = yaml.safe_load(file_content)
+
+    # parse the input data
+    aaa_config = intent_config['aaa_config']['commands']
+    ntp_config = intent_config['ntp_config']['commands']
+
+    # parse the device rules
+    device_role = intent_config['device_filter']['device_role']
+    device_family = intent_config['device_filter']['device_family']
+
+    logging.info(' Custom compliance settings from GitHub:')
+    logging.info('   aaa_config: \n' + aaa_config)
+    logging.info('   ntp_config: \n' + ntp_config)
+
+    # save the config compliance to files
+    with open(NETWORK_CONFIGS_PATH + 'aaa_config.txt', 'w') as f:
+        f.write(aaa_config)
+
+    # save the config compliance to files
+    with open(NETWORK_CONFIGS_PATH + 'ntp_config.txt', 'w') as f:
+        f.write(ntp_config)
+
+    logging.info(' Custom compliance device filter:')
+    logging.info('   device_role: ' + device_role)
+    logging.info('   device_family: ' + device_family)
+
+    # create a DNACenterAPI "Connection Object" to use the Python SDK
+    catalyst_center_api = DNACenterAPI(username=CATALYST_CENTER_USER, password=CATALYST_CENTER_PASS, base_url=CATALYST_CENTER_URL, version='2.3.5.3',
+                                       verify=False)
+
+    # collect device inventory
+    # get the device count
+    response = catalyst_center_api.devices.get_device_count()
+    device_count = response['response']
+    logging.info(' Number of devices managed by Cisco DNA Center: ' + str(device_count))
+
+    # get the device info list
+    offset = 1
+    limit = 500
+    device_list = []
+    while offset <= device_count:
+        response = catalyst_center_api.devices.get_device_list(offset=offset)
+        offset += limit
+        device_list.extend(response['response'])
+    logging.info(' Collected the device list from Cisco DNA Center')
+
+    # create device inventory, it will include all Catalyst Center device details
+
+    device_inventory = []
+    for device in device_list:
+        # select which inventory to add the device to
+        if device.family != "Unified AP":
+            device_id = device['id']
+            device_management_ip_address = device['managementIpAddress']
+
+            device_details = {'hostname': device['hostname']}
+            device_details.update({'device_ip': device['managementIpAddress']})
+            device_details.update({'device_id': device['id']})
+            device_details.update({'version': device['softwareVersion']})
+            device_details.update({'device_family': device['type']})
+            device_details.update({'role': device['role']})
+
+            # get the device site hierarchy
+            response = catalyst_center_api.devices.get_device_detail(identifier='uuid', search_by=device_id)
+            site = response['response']['location']
+            device_details.update({'site': site})
+
+            # get the device fabric role
+            device_sda_roles = []
+            try:
+                response = catalyst_center_api.sda.get_device_role_in_sda_fabric(
+                    device_management_ip_address=device_management_ip_address)
+                device_sda_roles = response['roles']
+            except:
+                pass
+            device_details.update({'sda_roles': device_sda_roles})
+
+            device_inventory.append(device_details)
+
+    logging.info(' Collected the device inventory from Cisco DNA Center')
+
+    # save device inventory to json formatted file
+    with open(NETWORK_CONFIGS_PATH + 'device_inventory.json', 'w') as f:
+        f.write(json.dumps(device_inventory, indent=4))
+    logging.info(' Saved the device inventory to file "device_inventory.json"')
+
+    os.chdir(NETWORK_CONFIGS_PATH)
+    # collect the device configs for the devices that match the role
+    logging.info(' Compliance checks for devices that match the device filter')
+    for item in device_inventory:
+        if item['role'] == device_role and item['device_family'] == device_family:
+            response = catalyst_center_api.devices.get_device_config_by_id(item['device_id'])
+            device_config = response['response']
+            with open(item['hostname'] + '_config.txt', 'w') as f:
+                f.write(device_config)
+            logging.info(' Saved the device config ' + item['hostname'] + '_config.txt')
+
+            logging.info(' Device: ' + item['hostname'] + ' AAA config check:')
+
+            # check common lines between the two files - custom compliance commands and running config
+            aaa_config_file = open('aaa_config.txt', 'r').readlines()
+            ntp_config_file = open('ntp_config.txt', 'r').readlines()
+            config_file = open(item['hostname'] + '_config.txt', 'r').readlines()
+
+            aaa_difference = difflib.Differ(charjunk=lambda x: x in [',', '.', '-', "'"])
+            aaa_commands_list = []
+            for line in aaa_difference.compare(aaa_config_file, config_file):
+                if line.startswith('-'):
+                    clean_line = line.strip()
+                    aaa_commands_list.append(clean_line)
+            if len(aaa_commands_list) == 0:
+                logging.info('    AAA config check passed')
+            else:
+                logging.info('    AAA config check failed, missing commands:')
+                for command in aaa_commands_list:
+                    logging.info('    ' + command)
+
+            ntp_difference = difflib.Differ(charjunk=lambda x: x in [',', '.', '-', "'"])
+            ntp_commands_list = []
+            for line in ntp_difference.compare(ntp_config_file, config_file):
+                if line.startswith('-'):
+                    clean_line = line.strip()
+                    ntp_commands_list.append(clean_line)
+            if len(ntp_commands_list) == 0:
+                logging.info('    NTP config check passed')
+            else:
+                logging.info('    NTP config check failed, missing commands:')
+                for command in ntp_commands_list:
+                    logging.info('    ' + command)
+
+    date_time = str(datetime.now().replace(microsecond=0))
+    logging.info(' End of Application "device_network_compliance.py" Run: ' + date_time)
+
+
+
+    return
+
+
+if __name__ == '__main__':
+    main()
